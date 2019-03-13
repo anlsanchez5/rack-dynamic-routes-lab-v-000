b271731662eab05286230ddc74204043194c3fa1
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-13 18:06:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,20 @@
 class Application
   @@items = [(Item.new("iphoneX", 900)),(Item.new("Water Bottle", 20))]
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path=="/items/"
       item_name = req.path.split("/items").last
-      item =
+      item = @@items.find{|i| i.name == item_name}
+
+      resp.write item.price
+    else
+      resp.write "Rount not found"
+      resp.status = 404
+    end
+
+    resp.finish
   end
 end
