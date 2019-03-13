0ad64dc41c6f9551b1b8bc6c25d77c5f1e3b7019
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-13 18:04:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,12 @@
 class Application
+  @@items = [(Item.new("iphoneX", 900)),(Item.new("Water Bottle", 20))]
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-
-
+    if req.path=="/items/"
+      item_name = req.path.split("/items").last
+      item =
+  end
 end
